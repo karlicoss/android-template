b4336602779a47933b1f1825ea80cb78100edc1e
Dima Gerasimov <karlicos@yandex-team.ru>
2016-11-03 14:17:55 +0300

    Fenum checker

--- android-template/src/main/java/com/github/karlicoss/template/MainActivity.java
+++ android-template/src/main/java/com/github/karlicoss/template/MainActivity.java
@@ -2,5 +2,34 @@ package com.github.karlicoss.template;
 
 import android.app.Activity;
 
+import org.checkerframework.checker.fenum.qual.Fenum;
+
+
 public class MainActivity extends Activity {
+
+    @SuppressWarnings("fenum:assignment.type.incompatible")   // initialization of fake enums
+    public class TestStatic {
+        public static final @Fenum("A") int ACONST1 = 1;
+        public static final @Fenum("A") int ACONST2 = 2;
+
+        public static final @Fenum("B") int BCONST1 = 4;
+        public static final @Fenum("B") int BCONST2 = 5;
+    }
+
+    static class FenumUser {
+        @Fenum("A") int state1 = TestStatic.ACONST1;     // ok
+        @Fenum("B") int state2 = TestStatic.ACONST1;     // Incompatible fenums forbidden!
+
+        void fenumArg(@Fenum("A") int p) {}
+
+        void foo() {
+            state1 = 4;                     // Direct use of value forbidden!
+            state1 = TestStatic.BCONST1;    // Incompatible fenums forbidden!
+            state1 = TestStatic.ACONST2;    // ok
+
+            fenumArg(5);                    // Direct use of value forbidden!
+            fenumArg(TestStatic.BCONST1);   // Incompatible fenums forbidden!
+            fenumArg(TestStatic.ACONST1);   // ok
+        }
+    }
 }
---
+++ run-checker.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./gradlew compileQaReleaseJavaWithJavac -Ptypecheck=true
